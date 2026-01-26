2ff788d2f2989533874d69c53ebfff79b968c7b7
nestoraguilar-ux <nestor.aguilar@uph.edu.hn>
2026-01-26 13:48:18 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+const tituloCancion = document.querySelector('.reproductor-musica h1');
+const nombreArtista = document.querySelector('.reproductor-musica p');
+
+const progreso = document.getElementById('progreso');
+const cancion = document.getElementById('cancion');
+
+const inconoControl = document.getElementById('iconoControl');
+const botonRepruducirPausar = document.querySelector('.controles button.boton-reproducir-pausar');
+
+const botonAtras = document.querySelector('.controles button.atras');
+const botonAdelante = document.querySelector('.controles button.adelante');
+
+const canciones = [
+    {
+        titulo:'Que Se Repita',
+        nombre:'Almighty',
+        fuente:'music/Almighty - Que Se Repita.mp3'
+    },
+    {
+        titulo:'BOKeTe',
+        nombre:'Bab Bunny',
+        fuente:'music/Bad Bunny - BOKeTE.mp3'
+    },
+    {
+        titulo:'Niño',
+        nombre:'Milo j',
+        fuente:'music/Milo j - Niño.mp3'
+    },
+];
+
+let indiceCancionActual = 0;
+
+function actualizarInfoCancion(){
+    tituloCancion.textContent = canciones[indiceCancionActual].titulo;
+    nombreArtista.textContent = canciones[indiceCancionActual].nombre;
+    cancion.src = canciones[indiceCancionActual].fuente;
+    cancion.addEventListener('loadeddata',function(){});
+};
+//cancion.addEventListener('loadedmetadata',function(){
+   // progreso.max = cancion.duration;
+   // progreso.value = cancion.currentTime;
+
+//}),
+
+botonRepruducirPausar.addEventListener('click',reproducirPausar);
+
+function reproducirPausar(){
+    if(cancion.paused){
+        reproducirCancion();
+    } else {
+        pausarCancion();
+    }
+};
+
+function reproducirCancion(){
+    cancion.play();
+    inconoControl.classList.add('bi-pause-fill')
+    inconoControl.classList.remove('bi-play-fill')
+}
+
+function pausarCancion(){
+    cancion.pause();
+    inconoControl.classList.remove('bi-pause-fill')
+    inconoControl.classList.add('bi-play-fill')
+}
+
+cancion.addEventListener('timeupdate',function(){
+    if(!cancion.paused){
+        progreso.value = cancion.currentTime;
+    }
+});
+ 
+progreso.addEventListener ('input',function(){
+    cancion.currentTime = progreso.value;
+});
+
+progreso.addEventListener('change',function(){
+     reproducirCancion();
+ });
+
+botonAdelante.addEventListener('click',function(){
+    indiceCancionActual = (indiceCancionActual  + 1) % canciones.length;
+    actualizarInfoCancion();
+    reproducirCancion;
+});
+
+botonAtras.addEventListener('click',function(){
+    indiceCancionActual = (indiceCancionActual - 1 + canciones.length) % canciones.length;
+    actualizarInfoCancion();
+    reproducirCancion;
+});
+
+actualizarInfoCancion();
+
+
+
